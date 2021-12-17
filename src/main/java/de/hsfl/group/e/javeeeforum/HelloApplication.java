41ef888c845b3c4bab1fe6cbc4be75165f9b97ae
Cedrik Hoffmann <cedrik.hoffmann@stud.hs-flensburg.de>
2021-12-17 13:55:35 +0100

    :tada: init commit

---
+++ src/main/java/de/hsfl/group/e/javeeeforum/HelloApplication.java
@@ -0,0 +1,9 @@
+package de.hsfl.group.e.javeeeforum;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
