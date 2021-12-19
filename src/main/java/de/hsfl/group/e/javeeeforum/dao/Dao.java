b11114ca33c6d22c06436888f4da7229252f878d
Cedrik Hoffmann <cedrik.hoffmann@stud.hs-flensburg.de>
2021-12-20 00:45:11 +0100

    Add Comments

--- src/main/java/de/hsfl/group/e/javeeeforum/dao/Dao.java
+++ src/main/java/de/hsfl/group/e/javeeeforum/dao/Dao.java
@@ -4,12 +4,26 @@ import javax.persistence.Query;
 import java.util.List;
 
 public interface Dao<T> {
+    // Get all from Database
     List<T> getAll();
-    List<T> getListOfQuery(Query query);
-    T getById(Long id);
-    T getElementOfQuery(Query query);
-    void addElement(T e);
-    void updateElement(T e);
-    void removeElement(T e);
 
+    // Get resultList by Query
+    // z.B.: "SELECT e FROM Creator e WHERE e.id >= 6"
+    List<T> getListOfQuery(Query query);
+
+    // Get one Element by id
+    T getById(Long id);
+
+    // Get one Element by Query
+    // z.B.: "SELECT e FROM Creator e WHERE e.username = 'mustermann'"
+    T getElementOfQuery(Query query);
+
+    // Add Element to Database
+    void addElement(T e);
+
+    // Update Element to Database
+    void updateElement(T e);
+
+    // Remove Element from Database
+    void removeElement(T e);
 }
