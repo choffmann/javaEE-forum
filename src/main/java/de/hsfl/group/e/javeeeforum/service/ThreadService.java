fc0369238ddd25d02a5f9d9bd559cf86dc182547
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2021-12-20 19:06:24 +0100

    created the createThread endpoint

--- src/main/java/de/hsfl/group/e/javeeeforum/service/ThreadService.java
+++ src/main/java/de/hsfl/group/e/javeeeforum/service/ThreadService.java
@@ -8,11 +8,16 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
 import de.hsfl.group.e.javeeeforum.dao.*;
+import de.hsfl.group.e.javeeeforum.dto.CategoryDto;
+import de.hsfl.group.e.javeeeforum.dto.TagDto;
 import de.hsfl.group.e.javeeeforum.dto.ThreadDto;
+import de.hsfl.group.e.javeeeforum.model.Category;
+import de.hsfl.group.e.javeeeforum.model.Creator;
+import de.hsfl.group.e.javeeeforum.model.Tag;
 import de.hsfl.group.e.javeeeforum.model.Thread;
 
 import java.net.URI;
-import java.util.List;
+import java.util.*;
 
 @Path("/threads")
 public class ThreadService {
@@ -22,6 +27,15 @@ public class ThreadService {
 
     @Inject
     ThreadDao threadDao;
+
+    @Inject
+    CreatorDao creatorDao;
+
+    @Inject
+    CategoryDao categoryDao;
+
+    @Inject
+    TagDao tagDao;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -44,6 +58,46 @@ public class ThreadService {
         return ThreadDto.fromModel(threadDao.getById(id));
     }
 
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createThread(ThreadDto threadDto, @QueryParam("creator") Long creatorID) {
+        Creator creator = creatorDao.getById(creatorID);
+        if (creator == null)
+            throw new WebApplicationException(
+                    Response.status(500).entity("The user was not found").build());
 
+        List<Category> categories = new LinkedList<>();
+        for (CategoryDto cdto : threadDto.getCategories()) {
+            Category category = categoryDao.getById(cdto.getId());
+            if (category == null)
+                throw new WebApplicationException(
+                        Response.status(500).entity("The category was not found").build());
+            categories.add(category);
+        }
+
+        List<Tag> tags = new LinkedList<>();
+        for (TagDto tdto : threadDto.getTags()) {
+            if (tdto.getTag() != null) {
+                Tag tag = new Tag();
+                tag.setTag(tdto.getTag());
+                tagDao.addElement(tag);
+                tags.add(tag);
+            }
+        }
+
+        Thread thread = new Thread();
+        thread.setCreator(creator);
+        thread.setTitle(threadDto.getTitle());
+        thread.setText(threadDto.getText());
+        thread.setCategories(categories);
+        thread.setTags(tags);
+        thread.setCreatedAt(Calendar.getInstance().getTime());
+        thread.setModifiedAt(Calendar.getInstance().getTime());
+
+        threadDao.addElement(thread);
+        URI location = uriInfo.getAbsolutePathBuilder()
+                .path("" + thread.getId()).build();
+        return Response.created(location).build();
+    }
 
 }
