dd6ff8b5f8737d752b6db5af226e446738b7f76e
Kajsa Heuer <kajsa.heuer@stud.hs-flensburg.de>
2021-12-21 12:09:56 +0100

    Created the endpoints for the categories

---
+++ src/main/java/de/hsfl/group/e/javeeeforum/service/CategoryService.java
@@ -0,0 +1,60 @@
+package de.hsfl.group.e.javeeeforum.service;
+
+import de.hsfl.group.e.javeeeforum.dao.CategoryDao;
+import de.hsfl.group.e.javeeeforum.dao.CreatorDao;
+import de.hsfl.group.e.javeeeforum.dto.CategoryDto;
+import de.hsfl.group.e.javeeeforum.model.Category;
+import de.hsfl.group.e.javeeeforum.model.Creator;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
+import java.util.List;
+
+@Path("/categories")
+public class CategoryService {
+
+    @Context
+    private UriInfo uriInfo;
+
+    @Inject
+    CategoryDao categoryDao;
+
+    @Inject
+    CreatorDao creatorDao;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<CategoryDto> getAll() {
+        return CategoryDto.fromModelList(categoryDao.getAll());
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createCategory(CategoryDto categoryDto, @QueryParam("creator") Long creatorID){
+        Creator creator = creatorDao.getById(creatorID);
+        if (creator == null || !creator.isAdmin())
+            throw new WebApplicationException(
+                    Response.status(404).entity("Not authenticated").build());
+
+        Category category = new Category();
+        category.setText(categoryDto.getText());
+
+        categoryDao.addElement(category);
+        URI location = uriInfo.getAbsolutePathBuilder()
+                .path("" + category.getId()).build();
+        return Response.created(location).build();
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public CategoryDto getCategory(@PathParam("id") long id){
+        return CategoryDto.fromModel(categoryDao.getById(id));
+    }
+
+}
