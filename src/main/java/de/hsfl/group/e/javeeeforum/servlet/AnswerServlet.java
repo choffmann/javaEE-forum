be826cf07fa84791a276fa12272976618a6256ef
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2021-12-29 11:17:32 +0100

    First logic in AnswerServlet, not currently working

--- src/main/java/de/hsfl/group/e/javeeeforum/servlet/AnswerServlet.java
+++ src/main/java/de/hsfl/group/e/javeeeforum/servlet/AnswerServlet.java
@@ -1,7 +1,5 @@
 package de.hsfl.group.e.javeeeforum.servlet;
 
-import de.hsfl.group.e.javeeeforum.dto.AnswerDto;
-import de.hsfl.group.e.javeeeforum.dto.ThreadDto;
 import org.glassfish.jersey.client.ClientConfig;
 
 import javax.servlet.ServletException;
@@ -11,12 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet(name = "answerServlet", value = "/answerServlet")
 public class AnswerServlet extends HttpServlet {
@@ -25,8 +23,17 @@ public class AnswerServlet extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String threadId = request.getParameter("threadid");
         String answerText = request.getParameter("answertext");
+        //String creator = request.getParameter("creator");
+        String creator = "Pascal";
+        System.out.println(threadId+" "+creator+" "+answerText);
         //TODO: Antwort posten
         WebTarget target = startConnection();
+        target.queryParam("creator",creator);
+        //Sendet die Antwort an den Server
+        Response serverResponse = target.path("threads/"+threadId+"/answers").request().accept(MediaType.APPLICATION_JSON).post(Entity.entity(answerText,MediaType.APPLICATION_JSON));
+        //Fragt die Seite neu ab, ggf. später mit der serverResponse URL umändern
+        request.setAttribute("threadid",threadId);
+        request.getRequestDispatcher("java/de/hsfl/group/e/javeeeforum/servlet/ThreadServlet.java").forward(request, response);
     }
     private WebTarget startConnection(){
         ClientConfig clientconfig = new ClientConfig();
